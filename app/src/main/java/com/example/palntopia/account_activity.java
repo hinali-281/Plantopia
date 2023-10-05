3d3bfb49cd13b7492cc86d96d149ebd6cd82915b
hinali-281 <138670455+hinali-281@users.noreply.github.com>
2023-10-06 02:28:51 +0530

    Lots of changes done

--- app/src/main/java/com/example/palntopia/account_activity.java
+++ app/src/main/java/com/example/palntopia/account_activity.java
@@ -7,15 +7,14 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class account_activity extends AppCompatActivity {
-    TextView account_username, account_phone, account_email;
+    TextView account_username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
         account_username = findViewById(R.id.account_username);
-        account_phone = findViewById(R.id.account_Phone);
-        account_email = findViewById(R.id.textView3);
+
 
         // Retrieve user details from the Intent
         Intent intent = getIntent();
@@ -26,8 +25,7 @@ public class account_activity extends AppCompatActivity {
 
             // Set user details to the TextViews
             account_username.setText(username);
-            account_phone.setText(phone);
-            account_email.setText(email);
+
         }
     }
 }
